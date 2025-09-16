9b7baae766a963e1b513d7c3d8f2546ea7649a20
Abby Conners <abbyconners@lawn-128-61-55-234.lawn.gatech.edu>
2025-09-16 09:28:04 -0400

    top movies purchased on the website

--- cart/views.py
+++ cart/views.py
@@ -4,6 +4,7 @@ from movies.models import Movie
 from .models import Order, Item
 from django.contrib.auth.decorators import login_required
 from .utils import calculate_cart_total
+from django.db.models import Sum
 
 # Create your views here.
 def add(request, id):
@@ -30,6 +31,18 @@ def index(request):
     template_data['movies_in_cart'] = movies_in_cart
     template_data['cart_total'] = cart_total
     return render(request, 'cart/index.html', {'template_data' : template_data})
+
+def top_purchases(request):
+    template_data = {}
+    template_data['title'] = 'Top Purchases'
+    top_movies = (
+        Item.objects
+        .values('movie__id', 'movie__name')  # group by movie
+        .annotate(total_quantity=Sum('quantity'))  # sum all quantities purchased
+        .order_by('-total_quantity')[:10]  # top 10
+    )
+    template_data['top_movies'] = top_movies
+    return render(request, 'cart/top_purchases.html', {'template_data' : template_data})
 
 @login_required
 def purchase(request):
